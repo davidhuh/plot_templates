ceb9762dce0a775a556bb0fd8e4ea0309fd2a77b
David Huh <dhuh@uw.edu>
2013-04-09 15:08:23 -0700

    exponentiated coefficients and confidence intervals

--- formatforplot.R
+++ formatforplot.R
@@ -1,7 +1,7 @@
 ## Utility function that extracts estimates for fitted regression models and outputs
 ## numeric and formatted string versions suitable for plotting
 
-formatforplot <- function(obj, parm.names=names(coef(obj)), model.name=deparse(substitute(obj)), rnd = 2) {
+formatforplot <- function(obj, parm.names=names(coef(obj)), model.name=deparse(substitute(obj)), eform=TRUE, rnd = 2) {
   ## load the appropriate library for reading the fitted regression objected
   obj.class <- class(obj)
   if (obj.class=="lme") require(lme4, quietly=TRUE)
@@ -22,20 +22,43 @@ formatforplot <- function(obj, parm.names=names(coef(obj)), model.name=deparse(s
                   ifelse(pval<0.005 & pval>=0.0015, "<.01",
                          ifelse(pval<0.0015 & pval>=0.0005, ".001", "<.001")))
   
-  ## output formatted results in both numeric and string formats
-  out <- data.frame(  model = model.name,
-                      param = parm.names,
-                      beta = round(beta, rnd),
-                      se = round(se, rnd),
-                      lower = round(lower, rnd),
-                      upper = round(upper, rnd),
-                      pval = round(pval, rnd),
-                      beta.s = rndtostr(beta, rnd),
-                      se.s = rndtostr(se, rnd),
-                      lower.s = rndtostr(lower, rnd),
-                      upper.s = rndtostr(upper, rnd),
-                      pval.s = pvalf,
-                      stringsAsFactors=FALSE)
   
+  ## output formatted results in both numeric and string formats
+  out <- data.frame(model = model.name,
+                    param = parm.names,
+                    beta = beta,
+                    se = se,
+                    lower = lower,
+                    upper = upper,
+                    pval = pval,
+                    beta.s = rndtostr(beta, rnd),
+                    se.s = rndtostr(se, rnd),
+                    lower.s = rndtostr(lower, rnd),
+                    upper.s = rndtostr(upper, rnd),
+                    pval.s = pvalf,
+                    stringsAsFactors=FALSE)
+
+  ## append exponentiated coeficients, if requested
+  if (eform==TRUE) {
+    beta.e <- exp(beta)
+    lower.e <- exp(lower)
+    upper.e <- exp(upper)
+    ci.e <- paste("[", rndtostr(lower.e,rnd), ", ", rndtostr(upper.e,rnd), "]", sep="")
+    
+    out0 <- data.frame(beta.e = beta.e,
+                       lower.e = lower.e,
+                       upper.e = upper.e,
+                       beta.s = rndtostr(beta, rnd),
+                       se.s = rndtostr(se, rnd),
+                       lower.s = rndtostr(lower, rnd),
+                       upper.s = rndtostr(upper, rnd),
+                       beta.e.s = rndtostr(beta.e, rnd),
+                       ci.e.s = ci.e,
+                       stringsAsFactors=FALSE)
+    
+    ## 
+    out <- cbind(out, out0)
+  }
+
   return(out)
 }
